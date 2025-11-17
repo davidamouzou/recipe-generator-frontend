467b7ad0f01e882329bee1ea955cac6ad6349cc8
David Amouzou <124218548+davidamouzou@users.noreply.github.com>
2025-11-17 12:32:44 +0100

    update url

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    base_url: 'https://recipe-gen-delta.vercel.app',
+    base_url: 'https://recipe-generator-backend-fwaf.onrender.com',
     request_headers: {
         'Content-Type': 'application/json',
         'api-key': 'jkhui'
